d05ed730f2ca5fc723e39d69a9bcf49e0e3639c7
eduardanv <maria.neves@sptech.school>
2022-05-27 11:05:31 -0300

    atualização da tabela Post no banco de dados, adicionando o campo fkUsuario. Atualizando também a modelagem com este novo campo.

--- Banco de Dados/script_newalternative.sql
+++ Banco de Dados/script_newalternative.sql
@@ -1,31 +1,31 @@
 create database NewAlternative;
+/*drop database newalternative;*/
 use NewAlternative;
-
-create table Post(
-idPost int primary key auto_increment,
-titulo varchar(45),
-conteudo varchar(2000)
-)auto_increment=2500;
 
 create table Usuario(
 idUsuario int primary key auto_increment,
 nome varchar (45),
 email varchar(30),
+senha varchar(10),
 dataNasc date,
 telefone char(20)
 );
 
-create table UsuarioPost(
+create table Post(
+idPost int primary key auto_increment,
+titulo varchar(45),
+descricao varchar(2000),
 fkUsuario int,
-fkPost int,
-foreign key (fkUsuario) references Usuario(idUsuario),
-foreign key (fkPost) references Post(idPost),
-dataPostagem datetime
-);
+foreign key (fkUsuario) references Usuario(idUsuario)
+)auto_increment=2500;
 
 create table Comentario(
 idComentario int primary key auto_increment,
 fkUsuario int,
+fkPost int,
 foreign key (fkUsuario) references Usuario(idUsuario),
+foreign key (fkPost) references Post(idPost),
 comentario varchar(250)
 );
+
+select * from Usuario;
