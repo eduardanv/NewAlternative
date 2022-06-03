a94c15dac0c142c7f1b01eb81e427394facb57bf
eduardanv <maria.neves@sptech.school>
2022-06-02 23:33:29 -0300

    alterando banco de dados, excluindo tabela comentario, acertando exibição do mural na pagina

--- Banco de Dados/script_newalternative.sql
+++ Banco de Dados/script_newalternative.sql
@@ -19,13 +19,13 @@ fkUsuario int,
 foreign key (fkUsuario) references Usuario(idUsuario)
 )auto_increment=2500;
 
-create table Comentario(
-idComentario int primary key auto_increment,
-fkUsuario int,
-fkPost int,
-foreign key (fkUsuario) references Usuario(idUsuario),
-foreign key (fkPost) references Post(idPost),
-comentario varchar(250)
-);
-
 select * from Usuario;
+
+insert into Usuario values (null,'Eduarda','eduarda@gmail.com', '123', '2002-03-17', '(+55) 11 97472-1438');
+
+insert into Post values (null, 'Malfeitona', 'A tatuadora brasileira que se orgulha de fazer desenho ‘feio’, 
+								Conhecida por seus desenhos propositalmente feios, Helen começou a tatuar por acaso. 
+								Ela e o noivo queriam fazer tatuagens, mas não podiam gastar muito. Resolveram investir em um kit mais em conta e fazer os desenhos em casa mesmo.
+                                O resultado não foi lá grandes coisa, mas ainda assim agradou não somente a eles mas como os amigos também, 
+                                que passaram a pedir tatuagens para Helen. No começo, ela levava como brincadeira, mas o negócio começou a ficar sério,
+                                e ela passou inclusive a cobrar pelo trabalho.', 1);
